051bd6700e51c3e42695436e43ac2941779b11f6
Dimitar Vatev <div007@gmail.com>
2018-11-26 15:04:24 +0200

    Conditions and tasks: from 01. to 04.

---
+++ 03.SOLID - Lab/01. CSharp-OOP-Advanced-SOLID-Lab-Skeleton/P01.Stream_Progress/Music.cs
@@ -0,0 +1,20 @@
+﻿namespace P01.Stream_Progress
+{
+    public class Music : IStreambele
+    {
+        private string artist;
+        private string album;
+
+        public Music(string artist, string album, int length, int bytesSent)
+        {
+            this.artist = artist;
+            this.album = album;
+            Length = length;
+            BytesSent = bytesSent;
+        }
+
+        public int Length { get; set; }
+
+        public int BytesSent { get; set; }
+    }
+}
---
+++ 03.SOLID - Lab/01. CSharp-OOP-Advanced-SOLID-Lab-Skeleton/P01.Stream_Progress/StreamProgressInfo.cs
@@ -0,0 +1,18 @@
+﻿namespace P01.Stream_Progress
+{
+    public class StreamProgressInfo
+    {
+        private IStreambele streamable;
+
+ 
+        public StreamProgressInfo(IStreambele streamable)
+        {
+            this.streamable = streamable;
+        }
+
+        public int CalculateCurrentPercent()
+        {
+            return (streamable.BytesSent * 100) / streamable.Length;
+        }
+    }
+}
