d81d0e7bd9e6c5c6bf79f2ec8da1d21b0f40c3a2
Dimitar Vatev <div007@gmail.com>
2018-11-29 15:20:39 +0200

    Conditions and tasks: from 01. to 03.

---
+++ 05.Generics - Lab/P01_Box/Box.cs
@@ -0,0 +1,29 @@
+﻿namespace BoxOfT
+{
+    using System.Collections.Generic;
+
+    public class Box<T>
+    {
+        private IList<T> list;
+
+        public Box()
+        {
+            list = new List<T>();
+        }
+
+        public int Count => list.Count;
+
+        public void Add(T element)
+        {
+            list.Add(element);
+        }
+
+        public T Remove()
+        {
+            T removed = list[Count - 1];
+            list.RemoveAt(Count - 1);
+            return removed;
+        }
+
+    }
+}
---
+++ 05.Generics - Lab/P03_Scale/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GenericScale
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var compare = new Scale<int>(7, 9);
+
+            Console.WriteLine(compare.GetHeavier());
+        }
+    }
+}
